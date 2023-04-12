afbab039156a65b6ef558b0cb5dd30d06cecb21e
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-04-12 18:26:09 -0300

    adding restaurant update method

--- src/main/java/com/example/onebite/controllers/RestauranteController.java
+++ src/main/java/com/example/onebite/controllers/RestauranteController.java
@@ -1,16 +1,20 @@
 package com.example.onebite.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.onebite.dto.RestauranteDTO;
-import com.example.onebite.entities.Restaurante;
 import com.example.onebite.services.RestauranteService;
 
 @RestController
@@ -21,14 +25,27 @@ public class RestauranteController {
 	private RestauranteService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Restaurante>> findAll() {
-		List<Restaurante> list = service.findAll();		
+	public ResponseEntity<List<RestauranteDTO>> findAll() {
+		List<RestauranteDTO> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<RestauranteDTO> findById(@PathVariable Long id) {
 		RestauranteDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<RestauranteDTO> insert(@RequestBody RestauranteDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<RestauranteDTO> update(@PathVariable Long id, @RequestBody RestauranteDTO dto) {
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
--- src/main/java/com/example/onebite/services/RestauranteService.java
+++ src/main/java/com/example/onebite/services/RestauranteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,44 @@ public class RestauranteService {
 	@Autowired
 	private RestauranteRepository repository;
 	
-	public List<Restaurante> findAll() {
-		return repository.findAll();
+	public List<RestauranteDTO> findAll() {
+		List<Restaurante> list = repository.findAll();
+		return list.stream().map(restaurante -> new RestauranteDTO(restaurante)).toList();
 	}
 	
 	public RestauranteDTO findById(Long id) {
 		Optional<Restaurante> obj = repository.findById(id);
 		Restaurante entity = obj.orElseThrow(() -> new EntityNotFoundException());
 		return new RestauranteDTO(entity);
+	}
+	
+	public RestauranteDTO insert(RestauranteDTO dto) {
+		Restaurante entity = new Restaurante();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new RestauranteDTO(entity);
+	}
+	
+	@Transactional
+	public RestauranteDTO update(Long id, RestauranteDTO dto) {
+		try {
+			Restaurante entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new RestauranteDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException();
+		}
+	}
+	
+	private void copyDtoToEntity(RestauranteDTO dto, Restaurante entity) {
+		if (dto.getName() != null)			
+			entity.setName(dto.getName());
+		if (dto.getTelefone() != null)		
+			entity.setTelefone(dto.getTelefone());
+		if (dto.getDescricao() != null)		
+			entity.setDescricao(dto.getDescricao());
 	}
 	
 }
