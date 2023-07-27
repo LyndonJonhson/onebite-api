c0f0c290c2f3803bf879ebfc02b2ed3058a0d595
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-07-26 22:26:13 -0300

    usuario controller

---
+++ src/main/java/com/example/onebite/api/controller/UsuarioController.java
@@ -0,0 +1,69 @@
+package com.example.onebite.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.onebite.api.assembler.UsuarioDTOAssembler;
+import com.example.onebite.api.dto.request.UsuarioRequestDTO;
+import com.example.onebite.api.dto.response.UsuarioResponseDTO;
+import com.example.onebite.domain.model.Usuario;
+import com.example.onebite.domain.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioDTOAssembler usuarioDTOAssembler;
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<UsuarioResponseDTO> findAll() {
+		List<Usuario> list = usuarioService.findAll();
+		return usuarioDTOAssembler.toCollectionDto(list);
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public UsuarioResponseDTO findById(@PathVariable Long id) {
+		Usuario entity = usuarioService.findById(id);
+		return usuarioDTOAssembler.toDto(entity);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public UsuarioResponseDTO insert(@Valid @RequestBody UsuarioRequestDTO dto) {
+		Usuario entity = usuarioService.insert(dto);
+		return usuarioDTOAssembler.toDto(entity);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public UsuarioResponseDTO update(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO dto) {
+		Usuario entity = usuarioService.update(id, dto);
+		return usuarioDTOAssembler.toDto(entity);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		usuarioService.delete(id);
+	}
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,6 +10,10 @@ insert into bairro(nome, cidade_id) values ('Ipanema', 2);
 insert into cozinha(nome) values ('Hamburgueria');
 insert into cozinha(nome) values ('Pizzaria');
 
+insert into usuario(nome, email, senha, data_cadastro, ativo) values ('Lyndon Jonhson', 'jonhson@gmail,com', 'jonhson123', Now(), true);
+insert into usuario(nome, email, senha, data_cadastro, ativo) values ('Willian Barbosa', 'willian@gmail,com', 'willian123', Now(), true);
+insert into usuario(nome, email, senha, data_cadastro, ativo) values ('Housemberg Donanvam', 'housemberg@gmail,com', 'housemberg123', Now(), true);
+
 insert into restaurante(nome, telefone, descricao, aberto, ativo, data_cadastro, cozinha_id, cep, logradouro, numero, bairro_id) values ('Point do Acai', '1111-1111', 'Descricao do Point do Acai', false, true, Now(), 1, '59000000', 'Rua 1', '1', 1);
 insert into restaurante(nome, telefone, descricao, aberto, ativo, data_cadastro, cozinha_id, cep, logradouro, numero, bairro_id) values ('O Gostosão', '2111-1111', 'Descrição do O Gostosão', false, true, Now(), 1, '59000000', 'Rua 2', '2', 1);
 insert into restaurante(nome, telefone, descricao, aberto, ativo, data_cadastro, cozinha_id, cep, logradouro, numero, bairro_id) values ('Tentação Lanches', '3111-1111', 'Descrição do Tentação Lanches', false, true, Now(), 2, '59000000', 'Rua 3', '3', 1);
