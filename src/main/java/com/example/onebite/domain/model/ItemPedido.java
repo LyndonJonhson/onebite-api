51ea950c740db3be82abe270908a1a6a1a9d93eb
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-07-27 17:30:50 -0300

    itemPedido entity

---
+++ src/main/java/com/example/onebite/domain/model/ItemPedido.java
@@ -0,0 +1,101 @@
+package com.example.onebite.domain.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ItemPedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false)
+	private BigDecimal precoUnitario;
+	
+	@Column(nullable = false)
+	private BigDecimal precoTotal;
+	
+	@Column(length = 500)
+	private String observacao;
+
+	public ItemPedido() {
+	}
+
+	public ItemPedido(Long id, Integer quantidade, BigDecimal precoUnitario, BigDecimal precoTotal, String observacao) {
+		this.id = id;
+		this.quantidade = quantidade;
+		this.precoUnitario = precoUnitario;
+		this.precoTotal = precoTotal;
+		this.observacao = observacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(BigDecimal precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	public BigDecimal getPrecoTotal() {
+		return precoTotal;
+	}
+
+	public void setPrecoTotal(BigDecimal precoTotal) {
+		this.precoTotal = precoTotal;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
