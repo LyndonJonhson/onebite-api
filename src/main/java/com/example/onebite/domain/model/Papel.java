f220cbbe23519ea4cd3650277849a44db8e1006c
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-07-26 21:06:50 -0300

    Papel entity

---
+++ src/main/java/com/example/onebite/domain/model/Papel.java
@@ -0,0 +1,44 @@
+package com.example.onebite.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Papel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+
+	public Papel() {
+	}
+
+	public Papel(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
