286d018fc0ffd0f1733be01b96e25d79e043b0fe
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-07-26 21:56:41 -0300

    usuario service

---
+++ src/main/java/com/example/onebite/domain/service/UsuarioService.java
@@ -0,0 +1,70 @@
+package com.example.onebite.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.onebite.api.assembler.UsuarioAssembler;
+import com.example.onebite.api.dto.request.UsuarioRequestDTO;
+import com.example.onebite.domain.enums.Mensagem;
+import com.example.onebite.domain.exception.EntidadeEmUsoException;
+import com.example.onebite.domain.exception.EntidadeNaoEncontradaException;
+import com.example.onebite.domain.exception.MensagemNaoCompreensivelException;
+import com.example.onebite.domain.model.Usuario;
+import com.example.onebite.domain.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private UsuarioAssembler usuarioAssembler;
+
+	@Transactional(readOnly = true)
+	public List<Usuario> findAll() {
+		return usuarioRepository.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public Usuario findById(Long id) {
+		return usuarioRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException(
+				String.format(Mensagem.ENTIDADE_NAO_ENCONTRADA.getMensagem(), id)));
+	}
+
+	@Transactional
+	public Usuario insert(UsuarioRequestDTO dto) {
+		try {
+			Usuario entity = usuarioAssembler.toEntity(dto);
+			return usuarioRepository.save(entity);
+		} catch (DataIntegrityViolationException e) {
+			throw new MensagemNaoCompreensivelException(Mensagem.MENSAGEM_NAO_COMPREENSIVEL.getMensagem());
+		}
+
+	}
+
+	@Transactional
+	public Usuario update(Long id, UsuarioRequestDTO dto) {
+		Usuario entity = usuarioRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException(
+				String.format(Mensagem.ENTIDADE_NAO_ENCONTRADA.getMensagem(), id)));
+		usuarioAssembler.copyToEntity(dto, entity);
+		return usuarioRepository.save(entity);
+
+	}
+
+	public void delete(Long id) {
+		try {
+			usuarioRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(String.format(Mensagem.ENTIDADE_NAO_ENCONTRADA.getMensagem(), id));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format(Mensagem.ENTIDADE_EM_USO.getMensagem(), id));
+		}
+	}
+
+}
