9d868855c9f039acd0936083434d2e217e3271b7
Lyndon Jonhson Cabral Filho <lyndon.jonhson.700@ufrn.edu.br>
2023-07-26 23:11:30 -0300

    Papel dtos

---
+++ src/main/java/com/example/onebite/api/dto/response/PapelResponseDTO.java
@@ -0,0 +1,28 @@
+package com.example.onebite.api.dto.response;
+
+import java.io.Serializable;
+
+public class PapelResponseDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
