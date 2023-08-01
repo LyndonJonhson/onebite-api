70e13709cfaaaece8ec26a52139c74fcf33ad553
jonhs <jonhs@DESKTOP-BPJ7LKG>
2023-08-01 20:38:05 -0300

    FormaPagamento entity

---
+++ src/main/java/com/example/onebite/domain/model/FormaPagamento.java
@@ -0,0 +1,64 @@
+package com.example.onebite.domain.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class FormaPagamento implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+
+	public FormaPagamento() {
+	}
+
+	public FormaPagamento(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FormaPagamento other = (FormaPagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
