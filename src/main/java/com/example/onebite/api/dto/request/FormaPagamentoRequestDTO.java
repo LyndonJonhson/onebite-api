25736cba6382584bf29b5d4c2d1ee3916c243df4
lyndon.jonhson.700 <lyndon.jonhson.700@ufrn.edu.br>
2023-08-17 19:09:47 -0300

    FormaPagamento DTOs

---
+++ src/main/java/com/example/onebite/api/dto/request/FormaPagamentoRequestDTO.java
@@ -0,0 +1,21 @@
+package com.example.onebite.api.dto.request;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+public class FormaPagamentoRequestDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@NotBlank
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
